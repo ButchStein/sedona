8a1bc6b36eae91abdb03bf81700cd0c925f003e9
ButchStein <butchbs1982@gmail.com>
2019-06-04 01:44:21 +0300

    update

--- source/js/script.js
+++ source/js/script.js
@@ -69,7 +69,24 @@ var nameValidityChecks = [
 			return input.value.length < 1;
 		},
 		invalidityMessage: 'Это поле обязательно к заполнению',
-		element: document.querySelector('#name .input-requirements li:nth-child(1)')
+		element: document.querySelector('label[for="name"] .input-requirements li:nth-child(1)')
+	},
+	{
+		isInvalid: function (input) {
+			let illegalCharacters = input.value.match(/[a-zA-Z]/g);
+			return illegalCharacters ? true : false;
+		},
+		invalidityMessage: 'Использование специальных символов и цифр недопустимо',
+		element: document.querySelector('label[for="name"] .input-requirements li:nth-child(2)')
+	}
+];
+var surnameValidityChecks = [
+	{
+		isInvalid: function (input) {
+			return input.value.length < 1;
+		},
+		invalidityMessage: 'Это поле обязательно к заполнению',
+		element: document.querySelector('label[for="surname"] .input-requirements li:nth-child(1)')
 	},
 	{
 		isInvalid: function (input) {
@@ -77,17 +94,48 @@ var nameValidityChecks = [
 			return illegalCharacters ? true : false;
 		},
 		invalidityMessage: 'Использование специальных символов и цифр недопустимо',
-		element: document.querySelector('#name .input-requirements li:nth-child(2)')
+		element: document.querySelector('label[for="surname"] .input-requirements li:nth-child(2)')
+	}
+];
+var fatherNameValidityChecks = [
+	{
+		isInvalid: function (input) {
+			return input.value.length < 1;
+		},
+		invalidityMessage: 'Это поле обязательно к заполнению',
+		element: document.querySelector('label[for="father_name"] .input-requirements li:nth-child(1)')
+	},
+	{
+		isInvalid: function (input) {
+			let illegalCharacters = input.value.match(/[^a-zA-Z]/g);
+			return illegalCharacters ? true : false;
+		},
+		invalidityMessage: 'Использование специальных символов и цифр недопустимо',
+		element: document.querySelector('label[for="father_name"] .input-requirements li:nth-child(2)')
 	}
 ];
 
-var inputs = document.querySelectorAll('.feedback-form__person-input');
+var nameInput = document.getElementById('name');
+var surnameInput = document.getElementById('surname');
+var fatherNameInput = document.getElementById('father_name');
+
+nameInput.CustomValidation = new CustomValidation(nameInput);
+nameInput.CustomValidation.validityChecks = nameValidityChecks;
+
+surnameInput.CustomValidation = new CustomValidation(surnameInput);
+surnameInput.CustomValidation.validityChecks = surnameValidityChecks;
+
+fatherNameInput.CustomValidation = new CustomValidation(fatherNameInput);
+fatherNameInput.CustomValidation.validityChecks = fatherNameValidityChecks;
+
+var inputs = document.querySelectorAll('input[type="text"]');
 var submit = document.querySelector('button[type="submit"]');
 var form = document.getElementById('feedback');
 
 function validate() {
 	for (var i = 0; i < inputs.length; i++) {
 		inputs[i].CustomValidation.checkInput();
+		console.log(inputs[i]);
 	}
 }
 
